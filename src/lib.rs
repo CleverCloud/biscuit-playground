c1a6761db7c403e56670b528672a2dcdaaf87560
Geoffroy Couprie <contact@geoffroycouprie.com>
2020-12-17 11:47:56 +0100

    add/remoe/enale/disable facts, rules and caveats

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use yew::prelude::*;
 use biscuit_auth::{token::Biscuit, crypto::KeyPair, error};
 use log::*;
 use rand::prelude::*;
+use std::default::Default;
 
 /*
 struct Model {
@@ -53,11 +54,19 @@ struct Model {
     token: Token,
 }
 
+enum Kind {
+    Fact,
+    Rule,
+    Caveat,
+}
+
 enum Msg {
     AddOne,
-    AddFact { index: usize },
-    AddRule { index: usize },
-    AddCaveat { index: usize },
+    AddElement { kind: Kind, block_index: usize, },
+    DeleteElement { kind: Kind, block_index: usize, element_index: usize, },
+    SetEnabled { enabled: bool, kind: Kind, block_index: usize, element_index: usize, },
+    Update { kind: Kind, block_index: usize, element_index: usize, value: String, },
+    None,
 }
 
 impl Component for Model {
@@ -68,10 +77,12 @@ impl Component for Model {
         token.authority.facts.push(Fact {
             data: "user(#authority, \"user_1234\")".to_string(),
             parsed: false,
+            enabled: true,
         });
         token.authority.facts.push(Fact {
             data: "hello".to_string(),
             parsed: false,
+            enabled: true,
         });
         token.generate();
         Self { link, value: 0, token }
@@ -80,28 +91,69 @@ impl Component for Model {
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::AddOne => self.value += 1,
-            Msg::AddFact { index } => {
-                if index == 0 {
-                    self.token.authority.facts.push(Fact::default());
+            Msg::AddElement { kind, block_index } => {
+                let block = if block_index == 0 {
+                    &mut self.token.authority
                 } else {
-                    self.token.blocks[index-1].facts.push(Fact::default());
+                    &mut self.token.blocks[block_index-1]
+                };
+
+                match kind {
+                    Kind::Fact => block.facts.push(Fact::default()),
+                    Kind::Rule => block.rules.push(Rule::default()),
+                    Kind::Caveat => block.caveats.push(Caveat::default()),
                 }
             },
-            Msg::AddRule { index } => {
-                if index == 0 {
-                    self.token.authority.rules.push(Rule::default());
+            Msg::DeleteElement { kind, block_index, element_index, } => {
+                let block = if block_index == 0 {
+                    &mut self.token.authority
                 } else {
-                    self.token.blocks[index-1].rules.push(Rule::default());
+                    &mut self.token.blocks[block_index-1]
+                };
+
+                match kind {
+                    Kind::Fact => {
+                        block.facts.remove(element_index);
+                    },
+                    Kind::Rule => {
+                        block.rules.remove(element_index);
+                    },
+                    Kind::Caveat => {
+                        block.caveats.remove(element_index);
+                    },
                 }
             },
-            Msg::AddCaveat { index } => {
-                if index == 0 {
-                    self.token.authority.caveats.push(Caveat::default());
+            Msg::SetEnabled { enabled, kind, block_index, element_index, } => {
+                let block = if block_index == 0 {
+                    &mut self.token.authority
                 } else {
-                    self.token.blocks[index-1].caveats.push(Caveat::default());
+                    &mut self.token.blocks[block_index-1]
+                };
+
+                match kind {
+                    Kind::Fact => block.facts[element_index].enabled = enabled,
+                    Kind::Rule => block.rules[element_index].enabled = enabled,
+                    Kind::Caveat => block.caveats[element_index].enabled = enabled,
                 }
             },
+            Msg::Update { kind, block_index, element_index, value, } => {
+                let block = if block_index == 0 {
+                    &mut self.token.authority
+                } else {
+                    &mut self.token.blocks[block_index-1]
+                };
+
+                match kind {
+                    Kind::Fact => block.facts[element_index].data = value,
+                    Kind::Rule => block.rules[element_index].data = value,
+                    Kind::Caveat => block.caveats[element_index].data = value,
+                }
+            },
+            Msg::None => {},
         }
+
+        self.token.generate();
+
         true
     }
 
@@ -136,60 +188,114 @@ impl Component for Model {
 }
 
 impl Model {
-    fn view_block(&self, id: usize, block: &Block) -> Html {
+    fn view_block(&self, block_index: usize, block: &Block) -> Html {
         html! {
             <li>
             { "Facts:" }
                 <ul>
-                    { for block.facts.iter().map(|fact| self.view_fact(fact)) }
-                    <button onclick=self.link.callback(move |_| Msg::AddFact { index: id })>{ "+" }</button>
+                    { for block.facts.iter().enumerate()
+                        .map(|(fact_index, fact)| self.view_fact(block_index, fact_index, fact))
+                    }
+                    <li><button onclick=self.link.callback(move |_| {
+                        Msg::AddElement { kind: Kind::Fact, block_index }
+                    })>{ "+" }</button></li>
                 </ul>
             { "Rules:" }
                 <ul>
-                    { for block.rules.iter().map(|rule| self.view_rule(rule)) }
-                    <button onclick=self.link.callback(move |_| Msg::AddRule { index: id })>{ "+" }</button>
+                    { for block.rules.iter().enumerate()
+                        .map(|(rule_index, rule)| self.view_rule(block_index, rule_index, rule))
+                    }
+                    <li><button onclick=self.link.callback(move |_| {
+                        Msg::AddElement { kind: Kind::Rule, block_index }
+                    })>{ "+" }</button></li>
                 </ul>
             { "Caveats:" }
                 <ul>
-                    { for block.caveats.iter().map(|caveat| self.view_caveat(caveat)) }
-                    <button onclick=self.link.callback(move |_| Msg::AddCaveat { index: id })>{ "+" }</button>
+                    { for block.caveats.iter().enumerate()
+                        .map(|(caveat_index, caveat)| self.view_caveat(block_index, caveat_index, caveat))
+                    }
+                    <li><button onclick=self.link.callback(move |_| {
+                        Msg::AddElement { kind: Kind::Caveat, block_index }
+                    })>{ "+" }</button></li>
                 </ul>
             </li>
         }
     }
 
-    fn view_fact(&self, fact: &Fact) -> Html {
+    fn view_fact(&self, block_index: usize, element_index: usize, fact: &Fact) -> Html {
+        let is_enabled = fact.enabled;
+
         html! {
             <li>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::DeleteElement { kind: Kind::Fact, block_index, element_index }
+                })>{ "-" }</button>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::SetEnabled { kind: Kind::Fact, enabled: !is_enabled, block_index, element_index }
+                })>{ "/" }</button>
                 <input
                     type="text"
                     size="50"
                     class= { if fact.parsed { "" } else { "parse_error" } }
-                    value = { fact.data.clone() } />
+                    value = { fact.data.clone() }
+                    disabled = if !fact.enabled { true } else { false }
+
+                    oninput=self.link.callback(move |e: InputData| {
+                        Msg::Update { kind: Kind::Fact, block_index, element_index, value: e.value }
+                    })
+                    />
            </li>
         }
     }
 
-    fn view_rule(&self, rule: &Rule) -> Html {
+    fn view_rule(&self, block_index: usize, element_index: usize, rule: &Rule) -> Html {
+        let is_enabled = rule.enabled;
+
         html! {
             <li>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::DeleteElement { kind: Kind::Rule, block_index, element_index }
+                })>{ "-" }</button>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::SetEnabled { kind: Kind::Rule, enabled: !is_enabled, block_index, element_index }
+                })>{ "/" }</button>
                 <input
                     type="text"
                     size="50"
                     class= { if rule.parsed { "" } else { "parse_error" } }
-                    value = { rule.data.clone() } />
+                    value = { rule.data.clone() }
+                    disabled = if !rule.enabled { true } else { false }
+
+                    oninput=self.link.callback(move |e: InputData| {
+                        Msg::Update { kind: Kind::Rule, block_index, element_index, value: e.value }
+                    })
+                    />
            </li>
         }
     }
 
-    fn view_caveat(&self, caveat: &Caveat) -> Html {
+    fn view_caveat(&self, block_index: usize, element_index: usize, caveat: &Caveat) -> Html {
+        let is_enabled = caveat.enabled;
+
         html! {
             <li>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::DeleteElement { kind: Kind::Caveat, block_index, element_index }
+                })>{ "-" }</button>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::SetEnabled { kind: Kind::Caveat, enabled: !is_enabled, block_index, element_index }
+                })>{ "/" }</button>
                 <input
                     type="text"
                     size="50"
                     class= { if caveat.parsed { "" } else { "parse_error" } }
-                    value = { caveat.data.clone() } />
+                    value = { caveat.data.clone() }
+                    disabled = if !caveat.enabled { true } else { false }
+
+                    oninput=self.link.callback(move |e: InputData| {
+                        Msg::Update { kind: Kind::Caveat, block_index, element_index, value: e.value }
+                    })
+                    />
            </li>
         }
     }
@@ -204,23 +310,57 @@ pub fn run_app() {
     App::<Model>::new().mount_to_body();
 }
 
-#[derive(Clone,Debug,Default)]
+#[derive(Clone,Debug)]
 struct Fact {
     pub data: String,
     pub parsed: bool,
+    pub enabled: bool,
 }
 
-#[derive(Clone,Debug,Default)]
+impl Default for Fact {
+    fn default() -> Self {
+        Fact {
+            data: String::new(),
+            parsed: true,
+            enabled: true,
+        }
+    }
+}
+
+#[derive(Clone,Debug)]
 struct Rule {
     pub data: String,
     pub parsed: bool,
+    pub enabled: bool,
 }
 
-#[derive(Clone,Debug,Default)]
+impl Default for Rule {
+    fn default() -> Self {
+        Rule {
+            data: String::new(),
+            parsed: true,
+            enabled: true,
+        }
+    }
+}
+
+#[derive(Clone,Debug)]
 struct Caveat {
     pub data: String,
     pub parsed: bool,
+    pub enabled: bool,
     pub succeeded: Option<bool>,
+}
+
+impl Default for Caveat {
+    fn default() -> Self {
+        Caveat {
+            data: String::new(),
+            parsed: true,
+            enabled: true,
+            succeeded: None,
+        }
+    }
 }
 
 #[derive(Clone,Debug,Default)]
@@ -239,57 +379,58 @@ struct Token {
 
 impl Token {
     fn generate(&mut self) {
-        info!("generate: {}", line!());
+        info!("generate token: {:?}", self);
         let mut rng: StdRng = SeedableRng::seed_from_u64(0);
-        info!("generate: {}", line!());
         let root = KeyPair::new(&mut rng);
-        info!("generate: {}", line!());
 
         let mut builder = Biscuit::builder(&root);
-        info!("generate: {}", line!());
 
         for fact in self.authority.facts.iter_mut() {
-            fact.parsed = builder.add_authority_fact(fact.data.as_str()).is_ok();
+            if fact.enabled {
+                fact.parsed = builder.add_authority_fact(fact.data.as_str()).is_ok();
+            }
         }
 
         for rule in self.authority.rules.iter_mut() {
-            rule.parsed = builder.add_authority_rule(rule.data.as_str()).is_ok();
+            if rule.enabled {
+                rule.parsed = builder.add_authority_rule(rule.data.as_str()).is_ok();
+            }
         }
 
         for caveat in self.authority.caveats.iter_mut() {
-            caveat.parsed = builder.add_authority_caveat(caveat.data.as_str()).is_ok();
+            if caveat.enabled {
+                caveat.parsed = builder.add_authority_caveat(caveat.data.as_str()).is_ok();
+            }
         }
-        info!("generate: {}", line!());
 
         let mut token = builder.build(&mut rng).unwrap();
-        info!("generate: {}", line!());
 
         for block in self.blocks.iter_mut() {
             let temp_keypair = KeyPair::new(&mut rng);
-        info!("generate: {}", line!());
             let mut builder = token.create_block();
-        info!("generate: {}", line!());
 
             for fact in block.facts.iter_mut() {
-                fact.parsed = builder.add_fact(fact.data.as_str()).is_ok();
+                if fact.enabled {
+                    fact.parsed = builder.add_fact(fact.data.as_str()).is_ok();
+                }
             }
 
             for rule in block.rules.iter_mut() {
-                rule.parsed = builder.add_rule(rule.data.as_str()).is_ok();
+                if rule.enabled {
+                    rule.parsed = builder.add_rule(rule.data.as_str()).is_ok();
+                }
             }
 
             for caveat in block.caveats.iter_mut() {
-                caveat.parsed = builder.add_caveat(caveat.data.as_str()).is_ok();
+                if caveat.enabled {
+                    caveat.parsed = builder.add_caveat(caveat.data.as_str()).is_ok();
+                }
             }
 
-        info!("generate: {}", line!());
             token = token.append(&mut rng, &temp_keypair, builder).unwrap();
-        info!("generate: {}", line!());
         }
 
-        info!("generate: {}", line!());
         self.biscuit = Some(token);
-        info!("generate: {}", line!());
     }
 
 }
