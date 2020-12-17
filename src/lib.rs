54127c1b2bd10f47afb66cfc77012c41ab6d6da1
Geoffroy Couprie <contact@geoffroycouprie.com>
2020-12-17 14:40:40 +0100

    display the verifier

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,12 @@ enum Msg {
     DeleteElement { kind: Kind, block_index: usize, element_index: usize, },
     SetEnabled { enabled: bool, kind: Kind, block_index: usize, element_index: usize, },
     Update { kind: Kind, block_index: usize, element_index: usize, value: String, },
+
+    AddVerifierElement { kind: Kind },
+    DeleteVerifierElement { kind: Kind, element_index: usize, },
+    SetVerifierEnabled { enabled: bool, kind: Kind, element_index: usize, },
+    VerifierUpdate { kind: Kind, element_index: usize, value: String, },
+
     None,
 }
 
@@ -45,6 +51,12 @@ impl Component for Model {
             parsed: false,
             enabled: true,
         });
+        token.authority.rules.push(Rule {
+            data: "*self($id) <- user(#authority, $id)".to_string(),
+            parsed: false,
+            enabled: true,
+        });
+        token.verifier.caveats.push(Caveat::new("*check_user() <- user(#authority, \"user_5678\")"));
         token.generate();
         Self { link, value: 0, token }
     }
@@ -123,6 +135,41 @@ impl Component for Model {
                     Kind::Caveat => block.caveats[element_index].data = value,
                 }
             },
+            Msg::AddVerifierElement { kind } => {
+                match kind {
+                    Kind::Fact => self.token.verifier.facts.push(Fact::default()),
+                    Kind::Rule => self.token.verifier.rules.push(Rule::default()),
+                    Kind::Caveat => self.token.verifier.caveats.push(Caveat::default()),
+                }
+            },
+            Msg::DeleteVerifierElement { kind, element_index, } => {
+                match kind {
+                    Kind::Fact => {
+                        self.token.verifier.facts.remove(element_index);
+                    },
+                    Kind::Rule => {
+                        self.token.verifier.rules.remove(element_index);
+                    },
+                    Kind::Caveat => {
+                        self.token.verifier.caveats.remove(element_index);
+                    },
+                }
+            },
+            Msg::SetVerifierEnabled { enabled, kind, element_index, } => {
+
+                match kind {
+                    Kind::Fact => self.token.verifier.facts[element_index].enabled = enabled,
+                    Kind::Rule => self.token.verifier.rules[element_index].enabled = enabled,
+                    Kind::Caveat => self.token.verifier.caveats[element_index].enabled = enabled,
+                }
+            },
+            Msg::VerifierUpdate { kind, element_index, value, } => {
+                match kind {
+                    Kind::Fact => self.token.verifier.facts[element_index].data = value,
+                    Kind::Rule => self.token.verifier.rules[element_index].data = value,
+                    Kind::Caveat => self.token.verifier.caveats[element_index].data = value,
+                }
+            },
             Msg::None => {},
         }
 
@@ -143,22 +190,25 @@ impl Component for Model {
 
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
-                <h2>{ "Biscuit Token" }</h2>
-                <ul>
-                    { self.view_block(0, &self.token.authority) }
-                    { (self.token.blocks.iter()
-                        .enumerate())
-                        .map(|(id, block)| self.view_block(id+1, block))
-                        .collect::<Html>() }
-                    <li><button onclick=self.link.callback(move |_| {
-                        Msg::AddBlock
-                    })>{ "+" }</button></li>
-                </ul>
-                <pre>
-                    { self.token.biscuit.as_ref().map(|b| b.print()).unwrap_or_else(String::new) }
-                </pre>
+                <div>
+                    <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                    <p>{ self.value }</p>
+                    <h2>{ "Biscuit Token" }</h2>
+                    <ul>
+                        { self.view_block(0, &self.token.authority) }
+                        { (self.token.blocks.iter()
+                            .enumerate())
+                            .map(|(id, block)| self.view_block(id+1, block))
+                            .collect::<Html>() }
+                        <li><button onclick=self.link.callback(move |_| {
+                            Msg::AddBlock
+                        })>{ "+" }</button></li>
+                    </ul>
+                    <pre>
+                        { self.token.biscuit.as_ref().map(|b| b.print()).unwrap_or_else(String::new) }
+                    </pre>
+                </div>
+                { self.view_verifier(&self.token.verifier) }
             </div>
         }
     }
@@ -290,6 +340,126 @@ impl Model {
            </li>
         }
     }
+
+    fn view_verifier(&self, verifier: &Verifier) -> Html {
+        html! {
+            <div>
+                <h3>{"Verifier"}</h3>
+            { "Facts:" }
+                <ul>
+                    { for verifier.facts.iter().enumerate()
+                        .map(|(fact_index, fact)| self.view_verifier_fact(fact_index, fact))
+                    }
+                    <li><button onclick=self.link.callback(move |_| {
+                        Msg::AddVerifierElement { kind: Kind::Fact }
+                    })>{ "+" }</button></li>
+                </ul>
+            { "Rules:" }
+                <ul>
+                    { for verifier.rules.iter().enumerate()
+                        .map(|(rule_index, rule)| self.view_verifier_rule(rule_index, rule))
+                    }
+                    <li><button onclick=self.link.callback(move |_| {
+                        Msg::AddVerifierElement { kind: Kind::Rule }
+                    })>{ "+" }</button></li>
+                </ul>
+            { "Caveats:" }
+                <ul>
+                    { for verifier.caveats.iter().enumerate()
+                        .map(|(caveat_index, caveat)| self.view_verifier_caveat(caveat_index, caveat))
+                    }
+                    <li><button onclick=self.link.callback(move |_| {
+                        Msg::AddVerifierElement { kind: Kind::Caveat }
+                    })>{ "+" }</button></li>
+                </ul>
+                <h4>{"Output"}</h4>
+                <p>{ match &verifier.error {
+                    Some(e) => format!("Error: {}", e),
+                    None => "Success".to_string(),
+                } }</p>
+                <pre>{ &verifier.output }</pre>
+
+            </div>
+        }
+    }
+
+    fn view_verifier_fact(&self, element_index: usize, fact: &Fact) -> Html {
+        let is_enabled = fact.enabled;
+
+        html! {
+            <li>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::DeleteVerifierElement { kind: Kind::Fact, element_index }
+                })>{ "-" }</button>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::SetVerifierEnabled { kind: Kind::Fact, enabled: !is_enabled, element_index }
+                })>{ "/" }</button>
+                <input
+                    type="text"
+                    size="50"
+                    class= { if fact.parsed { "" } else { "parse_error" } }
+                    value = { fact.data.clone() }
+                    disabled = if !fact.enabled { true } else { false }
+
+                    oninput=self.link.callback(move |e: InputData| {
+                        Msg::VerifierUpdate { kind: Kind::Fact, element_index, value: e.value }
+                    })
+                    />
+           </li>
+        }
+    }
+
+    fn view_verifier_rule(&self, element_index: usize, rule: &Rule) -> Html {
+        let is_enabled = rule.enabled;
+
+        html! {
+            <li>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::DeleteVerifierElement { kind: Kind::Rule, element_index }
+                })>{ "-" }</button>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::SetVerifierEnabled { kind: Kind::Rule, enabled: !is_enabled, element_index }
+                })>{ "/" }</button>
+                <input
+                    type="text"
+                    size="50"
+                    class= { if rule.parsed { "" } else { "parse_error" } }
+                    value = { rule.data.clone() }
+                    disabled = if !rule.enabled { true } else { false }
+
+                    oninput=self.link.callback(move |e: InputData| {
+                        Msg::VerifierUpdate { kind: Kind::Rule, element_index, value: e.value }
+                    })
+                    />
+           </li>
+        }
+    }
+
+    fn view_verifier_caveat(&self, element_index: usize, caveat: &Caveat) -> Html {
+        let is_enabled = caveat.enabled;
+
+        html! {
+            <li>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::DeleteVerifierElement { kind: Kind::Caveat, element_index }
+                })>{ "-" }</button>
+                <button onclick=self.link.callback(move |_| {
+                    Msg::SetVerifierEnabled { kind: Kind::Caveat, enabled: !is_enabled, element_index }
+                })>{ "/" }</button>
+                <input
+                    type="text"
+                    size="50"
+                    class= { if caveat.parsed { "" } else { "parse_error" } }
+                    value = { caveat.data.clone() }
+                    disabled = if !caveat.enabled { true } else { false }
+
+                    oninput=self.link.callback(move |e: InputData| {
+                        Msg::VerifierUpdate { kind: Kind::Caveat, element_index, value: e.value }
+                    })
+                    />
+           </li>
+        }
+    }
 }
 
 #[wasm_bindgen(start)]
@@ -318,6 +488,16 @@ impl Default for Fact {
     }
 }
 
+impl Fact {
+    pub fn new(s: &str) -> Self {
+        Fact {
+            data: s.to_string(),
+            parsed: true,
+            enabled: true,
+        }
+    }
+}
+
 #[derive(Clone,Debug)]
 struct Rule {
     pub data: String,
@@ -329,6 +509,16 @@ impl Default for Rule {
     fn default() -> Self {
         Rule {
             data: String::new(),
+            parsed: true,
+            enabled: true,
+        }
+    }
+}
+
+impl Rule {
+    pub fn new(s: &str) -> Self {
+        Rule {
+            data: s.to_string(),
             parsed: true,
             enabled: true,
         }
@@ -347,6 +537,17 @@ impl Default for Caveat {
     fn default() -> Self {
         Caveat {
             data: String::new(),
+            parsed: true,
+            enabled: true,
+            succeeded: None,
+        }
+    }
+}
+
+impl Caveat {
+    pub fn new(s: &str) -> Self {
+        Caveat {
+            data: s.to_string(),
             parsed: true,
             enabled: true,
             succeeded: None,
@@ -378,6 +579,7 @@ struct Token {
     pub authority: Block,
     pub blocks: Vec<Block>,
     pub biscuit: Option<Biscuit>,
+    pub verifier: Verifier,
 }
 
 impl Token {
@@ -435,7 +637,48 @@ impl Token {
             }
         }
 
+        self.verifier.verify(&token, root.public());
         self.biscuit = Some(token);
     }
+}
 
+#[derive(Clone,Debug,Default)]
+struct Verifier {
+    pub facts: Vec<Fact>,
+    pub rules: Vec<Rule>,
+    pub caveats: Vec<Caveat>,
+    pub error: Option<error::Token>,
+    pub output: String,
+}
+
+impl Verifier {
+    pub fn verify(&mut self, token: &Biscuit, root: PublicKey) {
+        self.error = None;
+
+        let mut verifier = token.verify(root).unwrap();
+
+        for fact in self.facts.iter_mut() {
+            if fact.enabled {
+                fact.parsed = verifier.add_fact(fact.data.as_str()).is_ok();
+            }
+        }
+
+        for rule in self.rules.iter_mut() {
+            if rule.enabled {
+                rule.parsed = verifier.add_rule(rule.data.as_str()).is_ok();
+            }
+        }
+
+        for caveat in self.caveats.iter_mut() {
+            if caveat.enabled {
+                caveat.parsed = verifier.add_caveat(caveat.data.as_str()).is_ok();
+            }
+        }
+
+        if let Err(e) = verifier.verify() {
+            self.error = Some(e);
+        }
+
+        self.output = verifier.print_world();
+    }
 }
